3172066515f9b1519ba99b2c56c6c2c293c862ee
Dakkshesh <dakkshesh5@gmail.com>
2024-06-01 18:42:43 +0530

    stdlib: Add ISO c23 functions
    Signed-off-by: Dakkshesh <dakkshesh5@gmail.com>

--- libgcompat/stdlib.c
+++ libgcompat/stdlib.c
@@ -126,4 +126,46 @@ unsigned long long int strtoull_l(const char *nptr, char **endptr, int base,
 unsigned long long strtouq(const char *nptr, char **endptr, int base)
 {
 	return strtoull(nptr, endptr, base);
+
+}
+
+/* ISO C23 function wrappers */
+long int __isoc23_strtol(const char *nptr, char **endptr, int base)
+{
+    return strtol(nptr, endptr, base);
+}
+
+unsigned long int __isoc23_strtoul(const char *nptr, char **endptr, int base)
+{
+    return strtoul(nptr, endptr, base);
+}
+
+long long int __isoc23_strtoll(const char *nptr, char **endptr, int base)
+{
+    return strtoll(nptr, endptr, base);
+}
+
+unsigned long long int __isoc23_strtoull(const char *nptr, char **endptr, int base)
+{
+    return strtoull(nptr, endptr, base);
+}
+
+long int __isoc23_strtol_l(const char *nptr, char **endptr, int base, locale_t loc)
+{
+    return strtol(nptr, endptr, base);
+}
+
+unsigned long int __isoc23_strtoul_l(const char *nptr, char **endptr, int base, locale_t loc)
+{
+    return strtoul(nptr, endptr, base);
+}
+
+long long int __isoc23_strtoll_l(const char *nptr, char **endptr, int base, locale_t loc)
+{
+    return strtoll(nptr, endptr, base);
+}
+
+unsigned long long int __isoc23_strtoull_l(const char *nptr, char **endptr, int base, locale_t loc)
+{
+    return strtoull(nptr, endptr, base);
 }
